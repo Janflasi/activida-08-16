21bba807987b50c9f864023f784b9f828e630bc5
juandiego <corporalmovimientos123@gmail.com>
2024-08-06 10:54:28 -0500

    Evidencia

---
+++ index.php
@@ -0,0 +1,5 @@
+<h1> Calificador Juan Diego Gonzalez Flaco</h1>
+
+<h2> Imagen en la caperta IMG</h2>
+<P> Calificado A JULIAN MENDEZ, SEBASTIAN AMAYA</P>
+<H5> MUY BUENA LA EXPOSICION ME ACLARARON TODAS MIS DUDAS SOBRE EL PROYETCO 8/10</H5>
